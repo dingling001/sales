7cd73e8acd35ed7b9bb2f149be57589690919fde
dl <215594238@qq.com>
2019-01-08 07:20:26 +0800

    1.8

--- pages/index/detail/detail.js
+++ pages/index/detail/detail.js
@@ -1,7 +1,7 @@
 // pages/index/detail/detail.js
 Page({
 
- 
+
   data: {
     imgUrls: [
       '../../img/g3.png',
@@ -10,44 +10,56 @@ Page({
     ],
   },
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.setNavigationBarTitle({
       title: 'GUCCI'
     })
   },
+  // 复制淘宝口令
+  copy_code() {
+    wx.setClipboardData({
+      data: '123',
+      success(res) {
+        wx.getClipboardData({
+          success(res) {
+            console.log(res.data) // data
+          }
+        })
+      }
+    })
+  },
 
-
-  onReady: function () {
+  onReady: function() {
 
   },
 
 
-  onShow: function () {
+  onShow: function() {
 
   },
 
 
-  onHide: function () {
-
-  },
-
- 
-  onUnload: function () {
+  onHide: function() {
 
   },
 
 
-  onPullDownRefresh: function () {
+  onUnload: function() {
 
   },
 
-  
-  onReachBottom: function () {
+
+  onPullDownRefresh: function() {
 
   },
 
-  
-  onShareAppMessage: function () {
+
+  onReachBottom: function() {
+
+  },
+
+
+  onShareAppMessage: function() {
 
   }
 })
