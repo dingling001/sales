b01f2389b83d0f722557475b38777ab58dc3079b
dingling <dingling>
2019-01-15 15:38:32 +0800

    寄售小程序

--- pages/class_detail/class_detail.js
+++ pages/class_detail/class_detail.js
@@ -3,34 +3,61 @@ Page({
 
   data: {
     i: 0,
-    show_brand: false
+    show_brand: false,
+    icon_status: false,
+    icon_status1: false,
   },
   // 综合
   all_fun() {
     this.setData({
       i: 0,
-      show_brand: false
+      show_brand: false,
+      icon_status: false,
+      icon_status1: false,
     })
   },
   // 寄售价
   consignment_fun() {
     this.setData({
       i: 1,
-      show_brand: false
+      show_brand: false,
+      icon_status1: false,
     })
+    if (this.data.icon_status == false) {
+      this.setData({
+        icon_status: true,
+      })
+    } else {
+      this.setData({
+        icon_status: false,
+      })
+    }
+
   },
   // 原价
   price_fun() {
     this.setData({
       i: 2,
-      show_brand: false
+      show_brand: false,
+      icon_status:false
     })
+    if (this.data.icon_status1 == false) {
+      this.setData({
+        icon_status1: true,
+      })
+    } else {
+      this.setData({
+        icon_status1: false,
+      })
+    }
   },
   // 品牌
   brand_fun() {
     this.setData({
       i: 3,
-      show_brand: true
+      show_brand: true,
+      icon_status: false,
+      icon_status1: false,
     })
   },
   // 选择品牌
@@ -39,11 +66,12 @@ Page({
       show_brand: false,
     })
   },
-  colse_mold(){
+  colse_mold() {
     this.setData({
       show_brand: false,
     })
   },
+
   onLoad: function(options) {
 
   },
--- pages/sale/sale_form/sale_form.js
+++ pages/sale/sale_form/sale_form.js
@@ -3,62 +3,46 @@ Page({
 
 
   data: {
+    img1: '../../img/g1.png',
+    img2: ''
+  },
+
+ 
+  onLoad: function(options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+ 
+  onReady: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onShow: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+ 
+  onHide: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+ 
+  onUnload: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+ 
+  onPullDownRefresh: function() {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+
+  onReachBottom: function() {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  
+  onShareAppMessage: function() {
 
   }
 })
