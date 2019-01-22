c1e86c0fb422ab760bd5daf2d13f4a801b809d1f
dingling <dingling>
2019-01-22 17:28:26 +0800

    寄售小程序1.22

---
+++ pages/my/my_address/my_address.js
@@ -0,0 +1,87 @@
+// pages/my/my_address/my_address.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    address_info: []
+  },
+
+  // 获取地址
+  getAddress() {
+    let that = this;
+    wx.chooseAddress({
+      success(res) {
+        console.log(res.userName)
+        console.log(res.postalCode)
+        console.log(res.provinceName)
+        console.log(res.cityName)
+        console.log(res.countyName)
+        console.log(res.detailInfo)
+        console.log(res.nationalCode)
+        console.log(res.telNumber)
+        that.setData({
+          address_info: res
+        })
+      }
+    })
+  },
+  saveAddress() {
+    wx.showToast({
+      title: '保存成功',
+    })
+  },
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
